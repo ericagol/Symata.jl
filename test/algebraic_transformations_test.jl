b44aa2cb86c7c3e5aa026c08f25912d9c85e3a7f
John Lapeyre <lapeyre@ribot>
2016-05-01 22:14:14 +0200

    check

--- test/algebraic_transformations_test.jl
+++ test/algebraic_transformations_test.jl
@@ -10,8 +10,18 @@
 @testex  Args(Collect(x^2 + y*x^2 + x*y + y + a*y, [x, y])) == [x * y,(1 + a) * y,(x ^ 2) * (1 + y)]
 @testex  Collect(a*Sin(2*x) + b*Sin(2*x), Sin(2*x)) == (a + b) * Sin(2 * x)
 @testex  Collect(a*x*Log(x) + b*(x*Log(x)), x*Log(x)) == (a + b) * x * Log(x)
+@testex  Collect(a*x^c + b*x^c, x) == a*(x^c) + b*(x^c)
+@testex  Collect(a*x^c + b*x^c, x^c) == (a + b)*(x^c)
 
 @testex Collect(a*x + b*y + c*x, x) == (a + c) * x + b * y
+@testex Collect(a*x^(2*c) + b*x^(2*c), x^c) == (a + b)*(x^(2*c))
+
+# sympy does not do this unless we use Expand. Mma does the expansion
+@testex Collect(Expand((1+a+x)^4), x) == 1 + 4*a + 6*(a^2) + 4*(a^3) + a^4 + (4 + 12*a + 12*(a^2) + 4*(a^3))*x + (6 + 12*a + 6*(a^2))*(x^2) + (4 + 4*a)*(x^3) + x^4
+
+
+
+
 
 #### Factor, Expand
 
--- test/output_test.jl
+++ test/output_test.jl
@@ -1,9 +1,10 @@
 # Test printing of expressions
 
 @testex testUserSyms
-
 @ex savestate = CompactOutput(True)
+
 @testex ToString( (a+b)*x ) == "(a + b)*x"
+
 
 @ex CompactOutput(savestate)
 @ex ClearAll(a,b,x,savestate)
